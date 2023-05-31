d0cd391f98b2e5f310f1475b55490343331b2c27
noofhamed9910 <135146592+noofhamed9910@users.noreply.github.com>
2023-05-31 17:57:38 +0100

    Create mainClientNoof21S20829.java

---
+++ mainClientNoof21S20829.java
@@ -0,0 +1,11 @@
+public class mainClientNoof21S20829 
+{
+  public static void main(String args[])
+    {
+        clientNoofhamed21S20829 NoofMcl =new clientNoofhamed21S20829();
+        Thread NoofMt = new Thread(NoofMcl);
+        
+        NoofMt.start();
+    }
+ }
+  
